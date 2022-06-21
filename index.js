ddcd093c11cddfd86f95186ce615fca9b70a7520
pletzjd <j.pletzer17@gmail.com>
2022-06-21 01:07:10 -0400

    Manually changed inquirer version to 6.5.2 from 9.0.0 as the newer version was returning an error when trying to run. Tested function recursion inside inquirer to make sure calling the function again would not cause any issues and using return would exit out of the function properly.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const inquirer = require('inquirer');
 const sql = require('mysql2');
 const cTable = require('console.table');
+
+let dontQuit = true;
+
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit'],
+                message: 'What would you like to do?',
+                name: 'action'
+            }
+        ])
+        .then((response)=>{
+            if(response.action === 'View all departments'){
+                mainMenu()
+            }else{
+                console.log('Bye')
+                return
+            }
+        })
+}
+
+mainMenu();
