f053e75f6031cecb42164a8ae0d6a54fd926524b
pletzjd <j.pletzer17@gmail.com>
2022-06-22 23:57:48 -0400

    Added functionality for "View all employees". Made sure the title of each column starts with a uppercase letter.

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function mainMenu() {
         ])
         .then((response)=>{
             if(response.action === 'View all departments'){
-                db.query(`SELECT department.name AS "Department Name", department.id AS "Department ID" FROM department`, (err, result) => {
+                db.query(`SELECT department.name AS "Department Name", department.id AS "Department ID" FROM department;`, (err, result) => {
                     if (err) {
                       console.log(err);
                     }
@@ -51,13 +51,21 @@ function mainMenu() {
                     continueYN();
                   });
             }else if(response.action === 'View all roles'){
-              db.query(`SELECT role.title, role.id AS "role id", department.name AS "department", role.salary FROM department INNER JOIN role ON department.id = role.department_id`, (err, result) => {
+              db.query(`SELECT role.title AS Title, role.id AS "Role ID", department.name AS Department, role.salary AS Salary FROM department INNER JOIN role ON department.id = role.department_id;`, (err, result) => {
                 if (err) {
                   console.log(err);
                 }
                 console.log(result);
                 continueYN();
               });
+            }else if(response.action === 'View all employees'){
+              db.query(`SELECT e.id AS ID, e.first_name AS "First Name", e.last_name AS "Last Name", role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee AS e LEFT JOIN role ON e.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee AS m ON e.manager_id = m.id ORDER BY id;`,(err, result) =>{
+                if (err) {
+                  console.log(err);
+                }
+                console.log(result);
+                continueYN();
+              })
             }
         })
 }
